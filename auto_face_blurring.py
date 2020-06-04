fd2f397e0e30e5478978dac91640d03f6c860413
Chaitanya Varier <cvvarier@gmail.com>
2020-06-04 08:35:46 -0400

    added processing ratio check to verify 40% speedup in execution time with GPU over CPU

--- auto_face_blurring.py
+++ auto_face_blurring.py
@@ -3,6 +3,7 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=C0103
 # pylint: disable=E1101
+from pymediainfo import MediaInfo
 import argparse
 import sys
 import time
@@ -33,7 +34,7 @@ def get_arguments():
     """
     parser = argparse.ArgumentParser(
         prog='Blur faces in video using openCV',
-        description='This module automatically applies a blurring mask to faces from a given input set of videos',
+        description='This module automatically applies a blurring mask to faces from a given input set of videos using a convolutional neural network.',
         epilog="Developed by: AutoDash (TODO: give credit to used sources)")
     parser.add_argument('-v', '--version', action='version', version='%(prog)s {}'.format(__version__))
     parser.add_argument('--input', default=DEFAULT_INPUT_PATH,
@@ -55,7 +56,7 @@ def get_arguments():
     parser.add_argument("--skip-frames", type=float, default=DEFAULT_SKIP_FRAMES,
                         help="The number of frames to skip after performing the ML prediction before predicting again. "
                              "This will help speed up the video processing. For every frame that is skipped, the blurring mask for the last calculated "
-                             "bounding boxes will remain."
+                             "bounding boxes will remain. "
                              "Default: {}".format(DEFAULT_SKIP_FRAMES))
 
     args = parser.parse_args()
@@ -136,7 +137,8 @@ def process_video(input_file, output_path, score_threshold, enlarge_factor, skip
     split_name = os.path.splitext(os.path.basename(input_file))
     input_file_name = split_name[0]
     input_file_extension = split_name[1]
-    output_file_name = os.path.join(output_path, '{}_blurred_auto_conf_{:3.2f}_skip{:d}{}'.format(input_file_name, score_threshold, skip_frames, input_file_extension))
+    output_file_name = os.path.join(output_path, '{}_blurred_auto_conf_{:3.2f}_skip{:d}{}'.format(input_file_name, 
+        score_threshold, skip_frames, input_file_extension))
 
     # If output directory doesn't exist, create it
     if not os.path.isdir(output_path):
@@ -183,8 +185,8 @@ def process_video(input_file, output_path, score_threshold, enlarge_factor, skip
                     [h, w] = frame.shape[:2]
                     out = cv2.VideoWriter(output_file_name, fourcc, 25.0, (w, h))
 
-                # Skip prediction on current frame if within skipping window and apply last calculated blurring masks (list of 
-                # masks for each face)
+                # Skip prediction on current frame if within skipping window and apply last calculated 
+                # blurring masks (list of masks for each face)
                 if count % (skip_frames + 1) != 0:
                     for i in range(len(last_bounding_boxes)):
                         bb = last_bounding_boxes[i]
@@ -225,10 +227,15 @@ def process_video(input_file, output_path, score_threshold, enlarge_factor, skip
 
                 out.write(frame)
             elapsed_time = time.time() - start_time
-            fps = count/elapsed_time
+            # Get input video length in s
+            media_info = MediaInfo.parse(input_file)
+            input_length = media_info.tracks[0].duration/1000
+            # Calculate processing ratio (s processing time/s video)
+            proc_ratio = elapsed_time/input_length
             w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
             h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-            print('Blurring video finished in {:.2f}s with fps {:2.4f} for video with dimensions {}x{}'.format(elapsed_time, fps, w, h))
+            print("Blurring video finished in {:.2f}s with processing ratio (s processing time/s video) {:2.4f} for video with dimensions {}x{}".format(elapsed_time, 
+                proc_ratio, w, h))
             print("Output saved at {}".format(output_file_name))
 
             cap.release()
@@ -241,6 +248,8 @@ def main():
     score_thresholds = args.score_thresholds.split(",")
     enlarge_factor = args.enlarge_factor
     skip_frames = int(args.skip_frames)
+
+    start_time = time.time()
 
     if os.path.isfile(input_path):
         if input_path.endswith(ACCEPTED_FILE_EXTENSION):
@@ -259,14 +268,27 @@ def main():
     files_processed = 0
     files_produced = 0
 
+    # Get total input video length in s
+    input_length = 0
+
     for file in os.listdir(input_path):
         if file.endswith(ACCEPTED_FILE_EXTENSION):
             for threshold in score_thresholds:
-                process_video(os.path.join(input_path, file), output_path, float(threshold), enlarge_factor, skip_frames)
+                input_file = os.path.join(input_path, file)
+                # Get total input video length in s
+                media_info = MediaInfo.parse(input_file)
+                input_length += media_info.tracks[0].duration/1000
+                # Process the video
+                process_video(input_file, output_path, float(threshold), enlarge_factor, skip_frames)
                 files_produced += 1
             files_processed +=1
+
+
+    elapsed_time = time.time() - start_time
+    proc_ratio = elapsed_time/input_length
     
-    print("{} files were processed and {} were produced.".format(files_processed, files_produced))
+    print("{} files were processed and {} were produced in {:.4f}s with average processing ratio "
+        "(s processing time/s video) {:2.4f}.".format(files_processed, files_produced, elapsed_time, proc_ratio))
 
 if __name__ == '__main__':
     main()
