01fdcbe75bc4bde00312f284291ef883756b6fe5
Chaitanya Varier <cvvarier@gmail.com>
2020-06-07 03:38:37 -0400

    disable TensorFlow warnings to reduce spam output

--- auto_face_blurring.py
+++ auto_face_blurring.py
@@ -10,8 +10,9 @@
 import argparse
 import time
 import numpy as np
-import tensorflow as tf
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
+import tensorflow as tf
 
 from threading import Thread
 
